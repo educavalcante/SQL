6fb0dab5f085331da90f7fa52c6b604fc759b35a
educavalcante <eduardo@cpcbrasil.com>
2022-05-20 00:51:04 -0300

    query venda lanches

--- consulta CR.sql
+++ consulta CR.sql
@@ -11,3 +11,16 @@ select cr.Prefixo, n.DescNatureza, s.nome, cr.Valor, cr.ValorPago, cr.Competenci
  join Socios s on s.CodSocio = cr.codsocio left 
  join Natureza n on  n.CodNatureza = cr.codnatureza
 where s.ativo <> 'N' and cr.valorpago < cr.valor;
+
+
+========
+ /*vendas lanche */
+select DataEmissao, PagadorLivre, s.nome, Competencia, Valor, ValorPago, cr.CodNatureza, n.DescNatureza
+ from ContasReceber cr 
+ left join Socios s on s.CodSocio = cr.codsocio
+ join Natureza n on n.CodNatureza = cr.CodNatureza
+ where cr.CodNatureza= 171 AND DataEmissao > '1.1.2022'
+ order by cr.CodContasReceber desc
+ 
+ /*natureza 171*/
+
